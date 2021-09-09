872200bb1eb11ba5b5bd6b83410afcf45ffc8ce9
Patrick Barringer <patrick.barringer@gmail.com>
2021-09-08 22:50:21 -0400

    Rough code added for the subgame HundredMeters

--- src/Main.elm
+++ src/Main.elm
@@ -8,9 +8,11 @@
 module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
+import Css exposing (..)
 import Die
-import Html exposing (button, div)
+import Html exposing (br, button, div)
 import Html.Events exposing (..)
+import Html.Styled.Attributes exposing (css)
 import List
 import Random
 import Svg exposing (..)
@@ -36,13 +38,27 @@ main =
 
 
 type alias Model =
-    { dice : List Die.Die
+    { rerolls : Int
+    , firstGroup : List Die.Die
+    , secondGroup : List Die.Die
+    , phase : Phase
+    , score : Int
     }
+
+
+type Phase
+    = One
+    | Two
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model (List.repeat 4 Die.blank)
+    ( { rerolls = 5
+      , firstGroup = List.repeat 4 Die.blank
+      , secondGroup = List.repeat 4 Die.blank
+      , phase = One
+      , score = 0
+      }
     , Cmd.none
     )
 
@@ -53,7 +69,10 @@ init _ =
 
 type Msg
     = Roll
+    | Reroll
     | NewDice (List Die.Die)
+    | FreezeOne
+    | FreezeTwo
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -61,11 +80,35 @@ update msg model =
     case msg of
         Roll ->
             ( model
-            , Random.generate NewDice (Random.list (List.length model.dice) Die.rolledDie)
+            , Random.generate NewDice (Random.list (List.length model.firstGroup) Die.rolledDie)
             )
 
+        Reroll ->
+            { model | rerolls = model.rerolls - 1 }
+                |> update Roll
+
         NewDice newDice ->
-            ( Model newDice
+            case model.phase of
+                One ->
+                    ( { model
+                        | firstGroup = newDice
+                        , score = getTotal newDice
+                      }
+                    , Cmd.none
+                    )
+
+                Two ->
+                    ( { model | secondGroup = newDice }
+                    , Cmd.none
+                    )
+
+        FreezeOne ->
+            ( { model | phase = Two }
+            , Cmd.none
+            )
+
+        FreezeTwo ->
+            ( { model | phase = Two }
             , Cmd.none
             )
 
@@ -86,7 +129,58 @@ subscriptions _ =
 view : Model -> Html.Html Msg
 view model =
     div []
-        (List.append
-            (List.map Die.toSvg model.dice)
-            [ button [ onClick Roll ] [ Html.text "Roll" ] ]
-        )
+        [ div []
+            [ Html.text ("Score:" ++ String.fromInt model.score)
+            , Html.text ("Rerolls: " ++ String.fromInt model.rerolls)
+            ]
+        , div []
+            (List.append
+                (List.map Die.toSvg model.firstGroup)
+                [ br [] []
+                , button [ onClick (rollOrReroll model) ] [ Html.text "Roll" ]
+                , button [ onClick FreezeOne ] [ Html.text "Freeze" ]
+                ]
+            )
+        , div []
+            (List.append
+                (List.map Die.toSvg model.secondGroup)
+                [ br [] []
+                , button [ onClick (rollOrReroll model) ] [ Html.text "Roll" ]
+                , button [ onClick FreezeTwo ] [ Html.text "Freeze" ]
+                ]
+            )
+        ]
+
+
+getTotal : List Die.Die -> Int
+getTotal dice =
+    List.sum (List.map Die.asInt dice)
+
+
+rollOrReroll : Model -> Msg
+rollOrReroll model =
+    let
+        firstDieVal =
+            model.firstGroup
+                |> List.head
+                |> Maybe.withDefault Die.blank
+                |> Die.asInt
+
+        secondDieVal =
+            model.secondGroup
+                |> List.head
+                |> Maybe.withDefault Die.blank
+                |> Die.asInt
+
+        phase =
+            model.phase
+    in
+    case ( phase, firstDieVal, secondDieVal ) of
+        ( One, 0, _ ) ->
+            Roll
+
+        ( Two, _, 0 ) ->
+            Roll
+
+        ( _, _, _ ) ->
+            Reroll
